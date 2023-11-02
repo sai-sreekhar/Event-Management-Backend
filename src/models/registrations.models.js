e5d57255babe903959a363d7c54002ef61162e5b
sai-sreekhar <sai.sreekhar@gmail.com>
2023-11-02 11:31:23 +0530

    feat: added models schema

---
+++ src/models/registrations.models.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const registrationSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Users",
+      required: true,
+    },
+    eventId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Events",
+      required: true,
+    },
+  },
+  { collection: "Registrations" }
+);
+
+module.exports = mongoose.model("Registrations", registrationSchema);
