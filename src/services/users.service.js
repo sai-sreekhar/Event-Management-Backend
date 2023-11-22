e1fb30824a94ecca5b695d4856c2ad52c87fe9db
sai-sreekhar <sai.sreekhar@gmail.com>
2023-11-22 14:32:17 +0530

    feat: added check for update details

--- src/services/users.service.js
+++ src/services/users.service.js
@@ -4,7 +4,16 @@ const { errorCodes } = require("./../utils/constants.utils");
 
 async function updateUser(userId, name, email, contact) {
   const user = await Users.findById(userId);
-
+  //the email and contact should be unique
+  const users = await Users.find({ $or: [{ email }, { contact }] });
+  if (users) {
+    throw new AppError(
+      "Email or Contact already exists",
+      403,
+      errorCodes.EMAIL_OR_CONTACT_ALREADY_EXISTS
+    );
+  }
+  
   const updatedUser = await Users.findByIdAndUpdate(
     userId,
     {
